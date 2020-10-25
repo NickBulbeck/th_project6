14205435e7c9c2ba559462bd6946caf8b4c57ecd
NickBulbeck <bulbecknick@gmail.com>
2020-10-25 17:48:49 +0000

    Added basic error-handling updates

---
+++ test/senseCheck.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+
+describe('Sense-check', function() {
+  it('Should run "npm test"', function() {
+    expect(true).to.be.true;
+    expect(false).to.be.false;
+  })
+})
